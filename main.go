80d6c2cbe70a10fde35410a60ba4e9bf9e1ec2be
unknown <teduckin@gmail.com>
2022-11-23 11:06:30 +1100

    added: goroutine for recieving messages

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ const (
     SERVER_TYPE = "tcp"
     SERVER_HOST = "localhost"
     SERVER_PORT = "8080"
+    SERVER_MSG_LENGTH = 512
 )
 
 func main() {
@@ -24,21 +25,34 @@ func main() {
     }
 
     // setup input scanner (fmt.Scanln does exactly opposite of what is says it does. awesome.)
-    input_scanner := bufio.NewScanner(os.Stdin)
+    input := bufio.NewScanner(os.Stdin)
+
+    // start thread to recieve messages on
+    go recieve_messages(server)
 
     // input loop
-    for
-    {
-        // scan for user input
-        input_scanner.Scan()
-        fmt.Printf("%s\n", input_scanner.Text())
+    for {
+        input.Scan()
 
         // write input to server
-        _, write_error := server.Write(input_scanner.Bytes())
+        _, write_error := server.Write(input.Bytes())
 
         if write_error != nil {
             fmt.Printf("failed to send data. (error: %s)\n", write_error)
             os.Exit(1)
         }
+    }
+}
+
+func recieve_messages(server net.Conn) {
+    for {
+        message := make([]byte, SERVER_MSG_LENGTH)
+        _, read_error := server.Read(message)
+
+        if read_error != nil {
+            fmt.Printf("failed to read message from the server. (error: %s)\n", read_error)
+        }
+
+        fmt.Printf("-> %s\n", message)
     }
 }
